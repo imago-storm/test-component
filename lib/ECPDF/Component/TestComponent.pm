816c83ec508fa9d5ad0474e0547f36e600e4b32b
Polina Shubina <925043@gmail.com>
2019-03-19 19:03:57 +0200

    initial commiy

---
+++ lib/ECPDF/Component/TestComponent.pm
@@ -0,0 +1,14 @@
+package ECPDF::Component::TestComponent;
+# use base 'ECPDF::Component';
+use strict;
+use warnings;
+
+sub init {
+    return 1;
+}
+
+sub doOOOOOOO {
+    print "OOOOOOOOOOO\n";
+}
+
+1;
